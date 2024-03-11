6e9d61ee6c64ab60b5b6e6142bf28be707c4886a
Marmarh <145338095+Marmarh@users.noreply.github.com>
2024-03-11 14:19:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-crud
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Marmarh/angular-crud)
---
+++ src/ app.module.ts
@@ -0,0 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: […],
+  imports: [
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
